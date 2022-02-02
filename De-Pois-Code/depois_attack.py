646c6c1d550c3273d73d3b31e9f3cd82d82ad304
Oschart <mohamed.ghanem@nabtahealth.com>
2022-02-02 01:06:17 +0100

    Added adv dataset saving

--- De-Pois-Code/depois_attack.py
+++ De-Pois-Code/depois_attack.py
@@ -25,13 +25,16 @@ from main import *
 from main import load_data
 from mimic_model_construction import *
 
-disable_eager_execution()
+enable_eager_execution()
+#disable_eager_execution()
 
 
 class DePoisAttack():
 	def __init__(self):
 		self.class_loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
+	def wasserstein_loss(self, y_true, y_pred):
+		return K.mean(y_true * y_pred)
 
 	def create_adv_pattern(self, model, img, label, model_type='critic'):
 		img = tf.cast(img, dtype=tf.float32)
@@ -40,9 +43,9 @@ class DePoisAttack():
 			tape.watch(img)
 			if model_type == 'critic':
 				prediction = model([img, label])
-				loss = model.wasserstein_loss(1, prediction)
+				loss = self.wasserstein_loss(1, prediction)
 			else:
-				prediction = model(img)
+				prediction = model.predict(img)
 				loss = self.class_loss(label, prediction)
 			
 		# Get the gradients of the loss w.r.t to the input image.
@@ -52,31 +55,34 @@ class DePoisAttack():
 
 		return signed_grad.numpy()
 
-	def craft_adv_dataset(self, model, D_src, model_type='critic', name='base_adv'):
-		adv_dataset_pth = f"data/adversarial/{name}.pkl"
+	def craft_adv_dataset(self, model, D_src, eps, model_type='critic', name='base_adv'):
+		adv_dataset_pth = f"data/adversarial/{name}_{model_type}_{eps}.pkl"
 		if os.path.isfile(adv_dataset_pth):
 			adv_dataset = pkl.load(open(adv_dataset_pth, "rb"))
 			return adv_dataset
 		
 		X_src, y_src = D_src
-		epsilons = [0.001, 0.01, 0.1, 0.5]
-		adv_dataset = {eps: [] for eps in epsilons}
+		X_adv = []
 		for img, label in tqdm(zip(X_src, y_src)):
 			img = np.expand_dims(img, axis=0)
 			label = np.expand_dims(label, axis=0)
 
 			perturbations = self.create_adv_pattern(model, img, label, model_type)
-			for eps in epsilons:
-				adv_x = img + perturbations*eps
-				adv_x = np.squeeze(np.clip(adv_x, -1, 1))
-	  	adv_dataset[eps].append((adv_x, label))
+			adv_x = img + perturbations*eps
+			adv_x = np.squeeze(np.clip(adv_x, -1, 1))
+			X_adv.append(adv_x)
+		
+		adv_dataset = [X_adv, y_src]
+		pkl.dump(adv_dataset, open(adv_dataset_pth, "wb"))
 		return adv_dataset
 
 	def clone_model(self, src_model, clone_model):
 		return
 
-	def wb_attack(self, depois_model, X_src):
-		return
+	def wb_attack(self, depois_model, D_src, eps):
+		adv_dataset_cr = self.craft_adv_dataset(depois_model.critic, D_src, eps, model_type='critic')
+		adv_dataset_cr_cl = self.craft_adv_dataset(depois_model.classifier, adv_dataset_cr, eps, model_type='classifier')
+
 	
 	def bb_attack(self, depois_model, X_src):
 		return
